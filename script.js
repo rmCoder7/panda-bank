b3639800afef92b118b744e8e3a927ca3fee1e7e
rmCoder7 <mohammedayon2021@gmail.com>
2023-09-14 20:02:26 +0600

    adda

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 document.getElementById("login-btn").addEventListener("click", function(){
 
+// email section
+
+const emailInput = document.getElementById("user-email");
+const emailOutput = emailInput.value;
+// console.log(emailOutput)
 
 
-    
+// password section
+
+const passInptu = document.getElementById('user-pass');
+const passOutput = passInptu.value;
+console.log(passOutput)
+
 })
